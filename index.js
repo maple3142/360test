0f69b3ef0900c6d8b36d1b0def75b67a1327b6cc
maple3142 <kirby741852963@gmail.com>
2018-03-31 20:26:06 +0800

    update

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const create360Viewer = require('360-image-viewer')
 const canvasFit = require('canvas-fit')
 
-function loadImg(id) {
-	const image = new Image()
-	image.src = `./${id}.jpg`
+const canvas = document.createElement('canvas')
+document.body.appendChild(canvas)
+const fit = canvasFit(canvas, window, window.devicePixelRatio)
+window.addEventListener('resize', fit, false)
+fit()
 
-	image.onload = () => {
-		const viewer = create360Viewer({ image })
+function loadImg(url, canvas) {
+	return new Promise((res, rej) => {
+		const image = new Image()
+		image.src = url
 
-		document.body.appendChild(viewer.canvas)
+		image.onload = () => {
+			const viewer = create360Viewer({ image, canvas })
 
-		const fit = canvasFit(viewer.canvas, window, window.devicePixelRatio)
-		window.addEventListener('resize', fit, false)
-		fit()
-		viewer.start()
-	}
+			viewer.start()
+			res(viewer)
+		}
+		image.onerror = err
+	})
 }
-
-let id = 0
-const MXID = 2
 
 const div = document.createElement('div')
 div.style.position = 'absolute'
@@ -26,20 +28,27 @@ div.style.top = '0px'
 div.style.left = '0px'
 div.style.zIndex = 10000
 document.body.appendChild(div)
-const prev = document.createElement('button')
-prev.textContent='prev'
-const next = document.createElement('button')
-next.textContent='next'
-prev.addEventListener('click', () => {
-	id--
-	if (id < 0) id = MXID
-	loadImg(id)
+;[1, 2, 3, 4].forEach(x => {
+	const btn = document.createElement('button')
+	btn.textContent = x
+	btn.addEventListener('click', () => loadImg(`./${x}.jpg`, canvas))
+	div.appendChild(btn)
 })
-next.addEventListener('click', () => {
-	id++
-	if (id > MXID) id = 0
-	loadImg(id)
+const upload = document.createElement('input')
+upload.type = 'file'
+upload.addEventListener('change', e => {
+	const [file] = e.target.files
+	if (!file) return
+	const reader = new FileReader()
+	reader.onload = e => {
+		const url = URL.createObjectURL(new Blob([e.target.result]))
+		const rurl = () => URL.revokeObjectURL(url)
+		loadImg(url, canvas)
+			.then(rurl)
+			.catch(rurl)
+	}
+	reader.readAsArrayBuffer(file)
 })
-div.appendChild(prev)
-div.appendChild(next)
-loadImg(id)
+div.appendChild(upload)
+
+loadImg('./1.jpg', canvas)
